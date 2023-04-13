9fe3e56e556d7366c301973882d8696af97aeafd
Sebastian Serrano <serranosebastian23@csu.fullerton.edu>
2023-04-13 01:59:55 -0700

    :white_check_mark: added settings.py
    added settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """
+    a class to store all settings for alien invasions
+    """
+    def __init__(self):
+        """
+        initialize the game's settings
+        """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
